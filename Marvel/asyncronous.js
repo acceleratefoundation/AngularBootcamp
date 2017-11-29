03b14ccecf5a12516c4d1c5d34a9b72f3696869c
devhulk <gerald.yerden@icloud.com>
2017-11-29 15:26:38 -0500

    added Marvel and other examples

---
+++ Marvel/asyncronous.js
@@ -0,0 +1,7 @@
+console.log('This is my first thing to be executed')
+
+setTimeout(function () {
+    console.log('this is my second thing to be executed')
+}, 2000)
+
+console.log('this is my last thing to be executed')
---
+++ Marvel/jarvis.js
@@ -0,0 +1,35 @@
+const api = require('marvel-api')
+const colors = require('colors')
+
+let marvel = api.createClient({
+  publicKey: '3ffd0da969c4817bd34a1fb7219d0707',
+  privateKey: '89bafbb9d7e4b778f536edd16e8a96ff74bb9508'
+})
+
+
+let getVision = () => {
+    marvel.characters.findByName('vision')
+    .then(console.log)
+    .fail(console.error)
+    .done()
+  }
+
+   let getAvengers = () => {
+    marvel.characters.findByName('thor')
+    .then((res) => {
+      let info = res.data[0].name + ' : ' + res.data[0].description
+      console.log(info.yellow)}
+    )
+    .fail(console.error)
+    .done()
+    
+    marvel.characters.findByName('spider-man')
+    .then((res) => {
+      let info = res.data[0].name + ' : ' + res.data[0].description
+      console.log(info.red)}
+    )
+    .fail(console.error)
+    .done()
+  }
+
+getAvengers()
